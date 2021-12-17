177bddd1adef0c16375b35429884d1d80617b6aa
Ziky-cell <va.fa.94@inbox.ru>
2021-12-17 14:38:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,60 @@
+﻿
+#include <iostream>  ////найти номер; последнего элемента, который меньше коли - чества положительных элементов.
+using namespace std;
+
+
+void fillArray(int* mass, int length);
+float findNum(int* mass, int length);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int* data;
+    int n;
+    cout << "Длинна массива: ";
+    cin >> n;
+    data = new int[n];
+
+    fillArray(data, n);
+    cout << endl << findNum(data, n);
+
+    delete[]data;
+    return 0;
+}
+
+void fillArray(int* mass, int length)
+{
+    cout << "Введите сам массив: ";
+
+    for (int i = 0; i < length; i++)
+    {
+        cin >> mass[i];
+    }
+}
+
+
+
+
+float findNum(int* mass, int length)
+{
+    int count = 0;
+    float result = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (mass[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        if (mass[i] < count)
+        {
+            result = i + 1;
+        }
+    }
+    cout << endl << "Номер элемента < длинны массива: ";
+
+    return result;
+}
